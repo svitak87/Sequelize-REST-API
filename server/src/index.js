53a230025f6095e0bdf07e490b0ad033552eec92
svitak87 <svitakwe87@hotmail.com>
2024-02-10 16:49:33 -0300

    test

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,17 @@
-const server = require('./server');
+const server = require("./server");
+const sequelize = require("./database/database");
 const PORT = 3001;
 
-server.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`)
-});
+const main = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+    server.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
 
+main();
