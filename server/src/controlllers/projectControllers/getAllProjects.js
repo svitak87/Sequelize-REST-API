d1c87d6ecd559363bd7a2807c9d43d20d001fdd7
svitak87 <svitakwe87@hotmail.com>
2024-02-11 03:28:11 -0300

    query

--- server/src/controlllers/projectControllers/getAllProjects.js
+++ server/src/controlllers/projectControllers/getAllProjects.js
@@ -1,9 +1,8 @@
-const { query } = require("express");
 const { Project } = require("../../database/database");
 
 const getAllProjects = async (query) => {
   try {
-    const projects = await Project.findAll({ where: query });
+    const projects = await Project.findAll({where: query});
 
     if (projects.length === 0) {
       throw new Error("There are no projects");
