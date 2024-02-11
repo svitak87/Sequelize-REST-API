8bae11dc73ed8858dc32452ab5a6c10a9cb24d79
svitak87 <svitakwe87@hotmail.com>
2024-02-10 22:56:53 -0300

    routes & controllers

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 const { USER, PASSWORD, HOST, PORT_DB, BDD } = process.env;
 
 const { Sequelize } = require("sequelize");
-const ProjectModel = require("../models/Project"); // Cambia el nombre de la variable
-const TaskModel = require("../models/Task"); // Cambia el nombre de la variable
+const ProjectModel = require("../models/Project"); 
+const TaskModel = require("../models/Task"); 
 
 const sequelize = new Sequelize(
   `postgres://${USER}:${PASSWORD}@${HOST}:${PORT_DB}/${BDD}`,
@@ -16,7 +16,6 @@ TaskModel(sequelize);
 
 const { Project, Task } = sequelize.models;
 
-// No olvides usar el nombre correcto de los modelos al llamar a hasMany y belongsTo
 Project.hasMany(Task, {
   foreignKey: "projectId",
 });
